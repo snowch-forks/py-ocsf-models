13683c54c426905a1b9e18852168223b21956c9b
Chris Snow <chris.snow@vastdata.com>
2025-06-13 13:38:08 +0000

    add authentication

---
+++ py_ocsf_models/objects/__init__.py
@@ -0,0 +1,97 @@
+"""OCSF Objects module."""
+
+from .account import Account
+from .actor import Actor
+from .analytic import Analytic
+from .api import API
+from .assessment import Assessment
+from .authentication_factor import AuthenticationFactor
+from .authentication_token import AuthenticationToken
+from .check import Check
+from .cloud import Cloud
+from .compliance import Compliance
+from .compliance_status import ComplianceStatus
+from .container import Container
+from .device import Device
+from .device_hardware_info import DeviceHardwareInfo
+from .dns_query import DNSQuery
+from .enrichment import Enrichment
+from .evidence_artifacts import EvidenceArtifacts
+from .finding_info import FindingInfo
+from .fingerprint import Fingerprint
+from .geolocation import GeoLocation
+from .group import Group
+from .image import Image
+from .kb_article import KBArticle
+from .kill_chain_phase import KillChainPhase
+from .ldap_person import LDAPPerson
+from .metadata import Metadata
+from .mitre_attack import MitreAttack
+from .network_endpoint import NetworkEndpoint
+from .network_interface import NetworkInterface
+from .observable import Observable
+from .operating_system import OperatingSystem
+from .organization import Organization
+from .policy import Policy
+from .process import Process
+from .product import Product
+from .related_event import RelatedEvent
+from .remediation import Remediation
+from .request_elements import RequestElements
+from .resource_details import ResourceDetails
+from .response_elements import ResponseElements
+from .service import Service
+from .session import Session
+from .url import URL
+from .user import User
+from .verdict import Verdict
+from .vulnerability_details import VulnerabilityDetails
+
+__all__ = [
+    "Account",
+    "Actor",
+    "Analytic",
+    "API",
+    "Assessment",
+    "AuthenticationFactor",
+    "AuthenticationToken",
+    "Check",
+    "Cloud",
+    "Compliance",
+    "ComplianceStatus",
+    "Container",
+    "Device",
+    "DeviceHardwareInfo",
+    "DNSQuery",
+    "Enrichment",
+    "EvidenceArtifacts",
+    "FindingInfo",
+    "Fingerprint",
+    "GeoLocation",
+    "Group",
+    "Image",
+    "KBArticle",
+    "KillChainPhase",
+    "LDAPPerson",
+    "Metadata",
+    "MitreAttack",
+    "NetworkEndpoint",
+    "NetworkInterface",
+    "Observable",
+    "OperatingSystem",
+    "Organization",
+    "Policy",
+    "Process",
+    "Product",
+    "RelatedEvent",
+    "Remediation",
+    "RequestElements",
+    "ResourceDetails",
+    "ResponseElements",
+    "Service",
+    "Session",
+    "URL",
+    "User",
+    "Verdict",
+    "VulnerabilityDetails",
+]
